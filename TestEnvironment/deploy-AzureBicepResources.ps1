3671df11e67f45bb2c41d3a13ada9d48ed9bb923
Marc Kean <marc@marckean.com>
2022-06-17 15:50:45 +1000

    .

--- TestEnvironment/deploy-AzureBicepResources.ps1
+++ TestEnvironment/deploy-AzureBicepResources.ps1
@@ -6,7 +6,7 @@ param (
 # All test resources, with the NSG and NSG rules as separate
 New-AzManagementGroupDeployment -Location $location -TemplateFile '.\TestEnvironment\main.bicep' -ManagementGroupId $ManagementGroupId
 
-# NSG with Rules - ONLY
+# NSG with rules included - ONLY
 New-AzManagementGroupDeployment -Location $location -TemplateFile '.\TestEnvironment\main(nsg).bicep' -ManagementGroupId $ManagementGroupId
 
 #az deployment mg create --location $location --management-group-id $ManagementGroupId --template-file 'C:\Users\makean\Documents\Github\AzureBicep\main.bicep' --verbose
